1207c7a799aae0bcd319e3a65d9969c3a5998fcc
dnabre <dnabre@happybunnylabs.com>
2023-10-04 09:28:59 -0600

    Stub function for svd function.

---
+++ svd.c
@@ -0,0 +1,17 @@
+/* svd.c: Perform a Singular Value Decomposition A = USV' of square matrix.
+ * see https://en.wikipedia.org/wiki/Singular_value_decomposition
+ *
+ * */
+
+#include <stdio.h>
+#include <math.h>
+
+
+/** Singular Value Decomposition A = USV' for square matrix A
+ *
+ **/
+
+void svd(double **A, double *S2, int n) {
+
+//TODO: add new implementation
+}
